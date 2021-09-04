e896b8a3d3e126e086bfa180a5ae00c8fd063382
Parrot <hitmansiege@gmail.com>
2021-09-04 05:21:31 +0200

    14:20 - Parrot BOT: Pushed into repo!

--- Assets/python/remPYC.py
+++ Assets/python/remPYC.py
@@ -4,3 +4,9 @@ import shutil
 def remPYC():
     if os.path.exists("Auth/__pycache__"):
         shutil.rmtree("Auth/__pycache__")
+
+    if os.path.exists("Assets/python/__pycache__"):
+        shutil.rmtree("Assets/python/__pycache__")
+
+    if os.path.exists("Assets/USTVGO/scripts/__pycache__"):
+        shutil.rmtree("Assets/USTVGO/scripts/__pycache__")
