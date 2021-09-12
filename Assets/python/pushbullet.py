582e616d4bcdaf082302b11d037374d7e8390a18
Parrot <hitmansiege@gmail.com>
2021-09-12 04:51:07 +0200

    13:50:30 - Parrot BOT: Pushed into repo!

--- Assets/python/pushbullet.py
+++ Assets/python/pushbullet.py
@@ -27,11 +27,13 @@ top = time + " ParrotBOT"
 
 if os.path.exists("me.txt"):
     with open ('me.txt', 'r') as fi:
-        global madeEPG
         madeEPG = fi.read()
 
-if str(madeEPG) == 'EPGhasBeenMade':
-    mid = "Pushed into repo! - with EPG"
+if os.path.exists("me.txt"):
+    if str(madeEPG) == 'EPGhasBeenMade':
+        mid = "Pushed into repo! - with EPG"
+    else:
+        mid = "Pushed into repo!"
 else:
     mid = "Pushed into repo!"
 
