6d1b7fe02a94cce7357c0f69bfa16e3539bce40a
Parrot <hitmansiege@gmail.com>
2021-09-30 08:19:16 +0200

    17:18:43 - Parrot BOT: Pushed into repo!

--- service.py
+++ service.py
@@ -2,6 +2,7 @@ from requests.models import MissingSchema
 from make import Clear, getUSTVGO, replaceUStVicons, MakeCS, MakeEng, MakeMain, Git, pushbulletMode, remPYC, RemoveMode2
 from Auth.auth import name, Email, gitToken, gitRepo
 import time
+import os
 
 token = gitToken
 repo = gitRepo
@@ -12,10 +13,17 @@ config_mail = "sudo git config --global user.email " + email
 config_name = "sudo git config --global user.name " + name
 
 
+def echo(msg):
+    echocmd = "sudo echo " + '"' + msg + '"'
+    os.system(echocmd)
+
 auto = True
 timeoutTime = open('Assets/Service/timeou.txt').read()
 int(timeoutTime)
 
+msg = "Timeout time is: " + str(timeoutTime)
+
+echo(msg)
 
 def Main():
     RemoveMode2()
@@ -32,4 +40,4 @@ def Main():
 
 while auto == True:
     Main()
-    time.sleep(timeoutTime)
+    time.sleep(int(timeoutTime))
