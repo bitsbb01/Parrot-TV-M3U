6d9965f5a469890867f62a48e2b9cb2288564a12
Parrot <hitmansiege@gmail.com>
2021-10-18 20:58:43 +0200

    Update

--- service.py
+++ service.py
@@ -7,21 +7,21 @@ import os
 
 try:
     from Auth.auth import gitToken, Email, name, gitRepo
+    token = str(os.environ['gitToken'])
+    email = str(os.environ['Email'])
+    name = str(os.environ['name'])
+    repo = str(os.environ['gitRepo'])
     if not gitRepo == "False":
-        token = str(os.environ['gitToken'])
-        email = str(os.environ['Email'])
-        name = str(os.environ['name'])
-        repo = str(os.environ['gitRepo'])
         origin = "sudo git remote set-url origin https://github:" + str(token) + str(repo) # Gets token and repo from Auth/auth.py
         config_mail = "sudo git config --global user.email " + email
         config_name = "sudo git config --global user.name " + name
     str(origin)
 except ModuleNotFoundError:
+    token = str(os.environ['gitToken'])
+    email = str(os.environ['Email'])
+    name = str(os.environ['name'])
+    repo = str(os.environ['gitRepo'])
     if not gitRepo == "False":
-        token = str(os.environ['gitToken'])
-        email = str(os.environ['Email'])
-        name = str(os.environ['name'])
-        repo = str(os.environ['gitRepo'])
         origin = "sudo git remote set-url origin https://github:" + str(token) + str(repo) # Gets token and repo from Auth/auth.py
         config_mail = "sudo git config --global user.email " + email
         config_name = "sudo git config --global user.name " + name
