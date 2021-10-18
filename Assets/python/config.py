ee02fa5719276558e722fef0f8e6adb07a1ab1a1
Parrot <hitmansiege@gmail.com>
2021-10-18 21:09:02 +0200

    Update

--- Assets/python/config.py
+++ Assets/python/config.py
@@ -1,12 +1,15 @@
 def config(replit, email, repo, token, name):
-    global origin
-    global config_mail
-    global config_name
     if replit == True:
+        global origin
+        global config_mail
+        global config_name
         origin = "git remote set-url origin https://github:" + str(token) + str(repo) # Gets token and repo from Auth/auth.py
         config_mail = "git config --global user.email " + email
         config_name = "git config --global user.name " + name
     elif replit == False:
+        global origin
+        global config_mail
+        global config_name
         origin = "sudo git remote set-url origin https://github:" + str(token) + str(repo) # Gets token and repo from Auth/auth.py
         config_mail = "sudo git config --global user.email " + email
         config_name = "sudo git config --global user.name " + name
